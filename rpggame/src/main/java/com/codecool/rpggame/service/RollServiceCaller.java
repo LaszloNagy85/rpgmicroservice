a7df8cd68454280955af034f8d11cb8446da910f
LaszloNagy85 <n.laszlo.uk@gmail.com>
2020-04-22 10:44:02 +0200

    initiall

---
+++ rpggame/src/main/java/com/codecool/rpggame/service/RollServiceCaller.java
@@ -0,0 +1,40 @@
+package com.codecool.rpggame.service;
+
+import com.codecool.rpggame.model.DiceRollResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.stream.Stream;
+
+@Service
+@Slf4j
+public class RollServiceCaller {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${diceroll.url}")
+    private String baseUrl;
+
+    public int rollForMonster() {
+        log.info("== Monster rolls ==");
+        DiceRollResult body = restTemplate.getForEntity(baseUrl + "/d20", DiceRollResult.class).getBody();
+        log.info("Service called on: " + body.getPort());
+        return body.getResult();
+    }
+
+    public int rollForHero() {
+        log.info("== Hero rolls ==");
+        return Stream.generate(() ->  restTemplate.getForEntity(baseUrl + "/d6", DiceRollResult.class).getBody())
+                .limit(3)
+                .mapToInt(value -> {
+                    log.info("Service called on: " + value.getPort());
+                    return value.getResult();
+                })
+                .sum();
+    }
+
+}
